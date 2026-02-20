6e8630077a1e02d22ee2f79b38dcbc542387b060
Tyler Peaster <tylerpeaster@gmail.com>
2026-02-19 23:22:05 -0600

    Added configs for the OptionsMenu page

--- OptionsMenu.Designer.cs
+++ OptionsMenu.Designer.cs
@@ -28,18 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            linkLabelDisplaySettings = new LinkLabel();
+            label1 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            checkBox1 = new CheckBox();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // linkLabelDisplaySettings
+            // 
+            linkLabelDisplaySettings.AutoSize = true;
+            linkLabelDisplaySettings.Location = new Point(12, 40);
+            linkLabelDisplaySettings.Name = "linkLabelDisplaySettings";
+            linkLabelDisplaySettings.Size = new Size(90, 15);
+            linkLabelDisplaySettings.TabIndex = 0;
+            linkLabelDisplaySettings.TabStop = true;
+            linkLabelDisplaySettings.Text = "Display Settings";
+            linkLabelDisplaySettings.LinkClicked += linkLabelDisplaySettings_LinkClicked;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(231, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Clock Widget Settings";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(checkBox1, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 7;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.Size = new Size(484, 361);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(245, 3);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
             // 
             // OptionsMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(484, 361);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(linkLabelDisplaySettings);
             Name = "OptionsMenu";
             Text = "OptionsMenu";
+            Load += OptionsMenu_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private LinkLabel linkLabelDisplaySettings;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private CheckBox checkBox1;
     }
 }
--- OptionsMenu.cs
+++ OptionsMenu.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Diagnostics;
 
 namespace ClockWidget
 {
@@ -15,30 +7,20 @@ namespace ClockWidget
         public OptionsMenu()
         {
             InitializeComponent();
+        }
 
-            this.MinimumSize = new Size(500, 400);
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.FormBorderStyle = FormBorderStyle.Sizable;
+        private void OptionsMenu_Load(object sender, EventArgs e)
+        {
 
-            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel
+        }
+
+        private void linkLabelDisplaySettings_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
             {
-                RowCount = 3,
-                ColumnCount = 2,
-                Dock = DockStyle.Fill,
-            };
-            tableLayoutPanel.Dock = DockStyle.Fill;
-
-            TextBox title = new TextBox
-            {
-                Text = "Clock Widget Options",
-                Font = new Font("Segoe UI", 18, FontStyle.Bold),
-                Dock = DockStyle.Fill,
-                ReadOnly = true,
-                BorderStyle = BorderStyle.None,
-                BackColor = this.BackColor,
-            };
-            Controls.Add(tableLayoutPanel);
-            tableLayoutPanel.Controls.Add(title, 0, 0);
+                FileName = "ms-settings:display",
+                UseShellExecute = true
+            });
         }
     }
 }
